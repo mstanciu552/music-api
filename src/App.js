d2e03a280de21cd8856c0348e5e59ccf04b92b9d
Mihai Stanciu <mstanciu552@gmail.com>
2021-01-12 18:35:15 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,86 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { clientID, clientSecret } from './credential';
+import ListGroup from 'react-bootstrap/ListGroup';
+import Form from 'react-bootstrap/Form';
 
-function App() {
+const App = () => {
+  const [token, setToken] = useState('');
+  const [categories, setCategories] = useState([]);
+  const [playlists, setPlaylists] = useState([]);
+  useEffect(() => {
+    axios('https://accounts.spotify.com/api/token', {
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        Authorization: 'Basic ' + btoa(clientID + ':' + clientSecret),
+      },
+      data: 'grant_type=client_credentials',
+      method: 'POST',
+    })
+      .then(res => {
+        setToken(res.data.access_token);
+        axios('https://api.spotify.com/v1/browse/categories?locale=sv_US', {
+          headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            Authorization: 'Bearer ' + res.data.access_token,
+          },
+          method: 'GET',
+        }).then(res => {
+          console.log(res.data.categories.items);
+
+          setCategories(res.data.categories.items);
+        });
+      })
+      .catch(err => console.error(err));
+  }, []);
+
+  const setValue = e => {
+    axios(
+      `https://api.spotify.com/v1/browse/categories/${e.target.value}/playlists?limit=10`,
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+        method: 'GET',
+      }
+    )
+      .then(res => {
+        setPlaylists(res.data.playlists.items);
+      })
+      .catch(err => console.error(err));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Form.Control
+        className="margin"
+        as="select"
+        name="Categories"
+        onChange={setValue}
+      >
+        {categories.map(category => (
+          <option key={category.id} value={category.id}>
+            {category.name}
+          </option>
+        ))}
+      </Form.Control>
+      {playlists ? (
+        <div className="padding">
+          <ListGroup>
+            {playlists.map((playlist, index) => (
+              <ListGroup.Item key={index}>
+                <a key={index} href={playlist.external_urls.spotify}>
+                  {playlist.name}
+                </a>
+              </ListGroup.Item>
+            ))}
+          </ListGroup>
+        </div>
+      ) : (
+        <h1>Please select playlist</h1>
+      )}
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/credential.js
@@ -0,0 +1,2 @@
+export const clientID = '0271966256de45b7982e179478b80144';
+export const clientSecret = 'a28bde7d0ce34d41893c7a1aef363394';
